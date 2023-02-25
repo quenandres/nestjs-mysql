2bea87f1606708280e2d98c34f13115c8ab5ff38
Jhonatan Mejia <quenandres@gmail.com>
2023-02-24 22:31:56 -0500

    Add: TypeORM

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
+
+## Setup Project
+Crear proyecto
+```bash
+nest new <name>
+```
+>
+
+Crear controller
+```bash
+nest g controller <name>
+```
+>
+
+Crear modulo
+```bash
+nest g module <name>
+```
+>
+
+Crear servicio
+```bash
+nest g service <name>
+```
+
+## TypeORM
+Instalación
+```bash
+npm install --save typeorm mysql2
+```
+
+#### _Comando Docker_
+```bash
+docker run --name mysql-nestjs -p 3380:3306 --restart unless-stopped -e "MYSQL_ROOT_PASSWORD=faztpassword" -e "MYSQL_DATABASE=nestdb" -d mysql
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3380,
+      username: 'root',
+      password: 'faztpasword',
+      database: 'nestdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, //Sincronizar los cambios en bd cuando se hagan los cambios en las clases.
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
